5d0a2311fa9a72baf80d81a4b7350af52ae9dd07
PCI-ACER\erhan <erhan.rgn04@gmail.com>
2022-08-06 01:07:36 +0300

    backend is completed

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// connet to db
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {})
+  .catch((error) => {
+    console.log(error);
+  });
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("connected to db & listening on port", process.env.PORT);
+});
